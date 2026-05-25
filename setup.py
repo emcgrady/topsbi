04ee50570a171a4a33c88cf5ffb9a2f426403a3d
Christopher McGrady <cmcgrad2@glados.crc.nd.edu>
2026-05-25 01:19:37 -0400

    pip setup script

---
+++ setup.py
@@ -0,0 +1,8 @@
+import setuptools
+
+setuptools.setup(
+    name='topsbi',
+    version='0.0.0',
+    description='Framework and tools to build and validate networks used for likelihood-free inference',
+    packages=setuptools.find_packages()
+)
